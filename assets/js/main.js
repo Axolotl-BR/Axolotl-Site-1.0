01a7b43cb001d48ecf169c31913d99b4057a7a06
Leoo <leononato2013@gmail.com>
2026-01-19 16:54:39 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 const reveals = document.querySelectorAll(".reveal");
 
-window.addEventListener("scroll",()=>{
+function revealOnScroll(){
+  const h = window.innerHeight * 0.85;
   reveals.forEach(el=>{
-    const top = el.getBoundingClientRect().top;
-    if(top < window.innerHeight - 80){
+    if(el.getBoundingClientRect().top < h){
       el.classList.add("active");
     }
   });
-});
+}
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
